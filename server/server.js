787aa070a2bccb52f33ff66764e4de18e49339ba
danielace1 <danielsudharsan1805@gmail.com>
2025-01-31 13:05:43 +0530

    feat: analytics api

--- server/server.js
+++ server/server.js
@@ -7,6 +7,8 @@ import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
 import couponRoutes from "./routes/coupon.route.js";
+import paymentRoutes from "./routes/payment.route.js";
+import analyticsRoutes from "./routes/analytics.route.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -18,9 +20,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/coupons", couponRoutes);
+app.use("/api/payments", paymentRoutes);
+app.use("/api/analytics", analyticsRoutes);
 
 app.get("/", (req, res) => {
-  res.json({ message: "Hi, This is Fusion Mart backend!" });
+  res.json({ message: "Hi, This is Fusion Mart server!" });
 });
 
 app.listen(PORT, () => {
